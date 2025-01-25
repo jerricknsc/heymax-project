768bbaa00d617faa15fc546a2c4e601a6ed5f54b
jerricknsc <jerricknsc@gmail.com>
2025-01-25 15:05:41 +0800

    formatting

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -3,28 +3,28 @@ const User = require('../models/UserModel.js'); // Import the User model
 
 // Function to fetch all users
 async function getAllUsers(req, res) {
-  try {
-    const users = await User.findAll(); // Fetch all users from the database
-    console.log('helo')
-    res.json(users);
-  }
-  catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error fetching users' });
-  }
+    try {
+        const users = await User.findAll(); // Fetch all users from the database
+        console.log('helo')
+        res.json(users);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error fetching users' });
+    }
 }
 
 // Function to create a new user
 async function addUser(req, res) {
-  try {
-    const { username, points } = req.body;
-    const newUser = await User.create({ username, points }); // Create a new user
-    res.status(201).json(newUser); // Return the newly created user
-  }
-  catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error creating user' });
-  }
+    try {
+        const { username, points } = req.body;
+        const newUser = await User.create({ username, points }); // Create a new user
+        res.status(201).json(newUser); // Return the newly created user
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error creating user' });
+    }
 }
 
 module.exports = { getAllUsers, addUser }; // Export controller functions
--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -3,17 +3,17 @@ const sequelize = require('./ConnectionManager'); // Import sequelize instance
 
 // Define the User model
 const User = sequelize.define('User', {
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    primaryKey: true,
-  },
-  points: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 0,
-  },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        primaryKey: true,
+    },
+    points: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+    },
 });
 
-module.exports = User; // Export the User model
+module.exports = User;
